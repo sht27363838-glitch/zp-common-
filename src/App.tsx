ce3c0f637ba75d48ba1e57e9e96ac58ae61e1c18
sht27363838-glitch <sht27363838@gmail.com>
2025-10-04 13:13:26 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import C0 from './pages/C0'
 import C1 from './pages/C1'
 import C2 from './pages/C2'
 import C4 from './pages/C4'
+import C5 from './pages/C5'
 import C6 from './pages/C6'
 import DQ from './pages/DQ'
 import Tools from './pages/Tools'
-import C5 from './pages/C5'
+import Data from './pages/Data'
+import Report from './pages/Report'
 import './styles/tokens.css'
 
 export default function App(){
@@ -19,9 +21,11 @@ export default function App(){
         <NavLink to='/growth' className={({isActive})=>isActive?'active':''}>C1 유입</NavLink>
         <NavLink to='/commerce' className={({isActive})=>isActive?'active':''}>C2 전환</NavLink>
         <NavLink to='/rewards' className={({isActive})=>isActive?'active':''}>C4 보상엔진</NavLink>
-        <NavLink to='/experiments' className={({isActive})=>isActive?'active':''}>C6 실험</NavLink>
         <NavLink to='/ops' className={({isActive})=>isActive?'active':''}>C5 운영</NavLink>
+        <NavLink to='/experiments' className={({isActive})=>isActive?'active':''}>C6 실험</NavLink>
         <NavLink to='/decisions' className={({isActive})=>isActive?'active':''}>DQ 결정큐</NavLink>
+        <NavLink to='/data' className={({isActive})=>isActive?'active':''}>데이터</NavLink>
+        <NavLink to='/report' className={({isActive})=>isActive?'active':''}>주간리포트</NavLink>
         <NavLink to='/tools' className={({isActive})=>isActive?'active':''}>도구</NavLink>
       </div>
       <Routes>
@@ -29,9 +33,11 @@ export default function App(){
         <Route path='/growth' element={<C1/>}/>
         <Route path='/commerce' element={<C2/>}/>
         <Route path='/rewards' element={<C4/>}/>
-        <Route path='/experiments' element={<C6/>}/>
         <Route path='/ops' element={<C5/>}/>
+        <Route path='/experiments' element={<C6/>}/>
         <Route path='/decisions' element={<DQ/>}/>
+        <Route path='/data' element={<Data/>}/>
+        <Route path='/report' element={<Report/>}/>
         <Route path='/tools' element={<Tools/>}/>
       </Routes>
     </div>
