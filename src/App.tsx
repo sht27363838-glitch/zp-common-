6e294db6980df2ed51bf4302d6b49dce3c9dadaf
sht27363838-glitch <sht27363838@gmail.com>
2025-10-04 12:56:45 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+// src/App.tsx
 import React from 'react'
 import { NavLink, Routes, Route } from 'react-router-dom'
 import C0 from './pages/C0'
@@ -8,6 +8,7 @@ import C4 from './pages/C4'
 import C6 from './pages/C6'
 import DQ from './pages/DQ'
 import Tools from './pages/Tools'
+import C5 from './pages/C5'
 import './styles/tokens.css'
 
 export default function App(){
@@ -19,6 +20,7 @@ export default function App(){
         <NavLink to='/commerce' className={({isActive})=>isActive?'active':''}>C2 전환</NavLink>
         <NavLink to='/rewards' className={({isActive})=>isActive?'active':''}>C4 보상엔진</NavLink>
         <NavLink to='/experiments' className={({isActive})=>isActive?'active':''}>C6 실험</NavLink>
+        <NavLink to='/ops' className={({isActive})=>isActive?'active':''}>C5 운영</NavLink>
         <NavLink to='/decisions' className={({isActive})=>isActive?'active':''}>DQ 결정큐</NavLink>
         <NavLink to='/tools' className={({isActive})=>isActive?'active':''}>도구</NavLink>
       </div>
@@ -28,6 +30,7 @@ export default function App(){
         <Route path='/commerce' element={<C2/>}/>
         <Route path='/rewards' element={<C4/>}/>
         <Route path='/experiments' element={<C6/>}/>
+        <Route path='/ops' element={<C5/>}/>
         <Route path='/decisions' element={<DQ/>}/>
         <Route path='/tools' element={<Tools/>}/>
       </Routes>
